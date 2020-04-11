24f9f9a2903b8370035a6a0f24b5f8f81fde9e23
upanddownagain <musil.adam@gmail.com>
2020-04-11 21:48:49 +0200

    oprava preferences, pridan system.gc do processasync

--- app/src/main/java/musil/adam/trackingiron/ProcessAsync.java
+++ app/src/main/java/musil/adam/trackingiron/ProcessAsync.java
@@ -24,6 +24,7 @@ public class ProcessAsync extends AsyncTask<Void,Void,Void> {
 
     @Override
     protected void onPreExecute() {
+        System.gc();
         progressBarRef.get().setVisibility(View.VISIBLE);
     }
 
@@ -39,7 +40,6 @@ public class ProcessAsync extends AsyncTask<Void,Void,Void> {
 
     @Override
     protected void onPostExecute(Void aVoid) {
-        //System.gc();
         progressBarRef.get().setVisibility(View.GONE);
         final Uri processedVid = videoProcessRef.get().getProcessedVid();
         Intent videoIntent = new Intent(contextRef.get().getApplicationContext(), VideoActivity.class);
