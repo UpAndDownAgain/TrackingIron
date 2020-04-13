18bc79904e6332221ae7d10384d908631e610b7e
upanddownagain <musil.adam@gmail.com>
2020-04-13 14:54:43 +0200

    viewmodel

---
+++ app/src/main/java/musil/adam/trackingiron/VideoViewModel.java
@@ -0,0 +1,32 @@
+package musil.adam.trackingiron;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class VideoViewModel extends AndroidViewModel {
+
+    private VideoRepository repository;
+    private LiveData<List<Video>> allVids;
+
+
+    public VideoViewModel(@NonNull Application application) {
+        super(application);
+
+        repository = new VideoRepository(application);
+        allVids = repository.getAllVids();
+
+    }
+
+    LiveData<List<Video>> getAllVids(){
+        return allVids;
+    }
+
+    public void insert(Video vid){
+        repository.insert(vid);
+    }
+}
